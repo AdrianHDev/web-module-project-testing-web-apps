e040ba3f5fa3026712ddead9a9dc91d07aa1c23c
AdrianHDev <adrianhensondev@gmail.com>
2021-06-03 14:08:24 -0400

    Change up formatting & fix async calls

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
+import {
+  getAllByTestId,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import ContactForm from "./ContactForm";
@@ -16,24 +21,53 @@ test("renders the contact form header", () => {
 
 test("renders ONE error message if user enters less then 5 characters into firstname.", async () => {
   let contactNode = render(<ContactForm />);
-  let userField = contactNode.getByLabelText(/First Name\*/i);
+  let firstnameField = contactNode.getByLabelText(/First Name\*/i);
   let submitButton = contactNode.getByTestId(/submitTest/i);
-  await (() => {
-    userEvent.type(userField, "Yes");
-    userEvent.click(submitButton);
-  })();
-  let errorMsg = contactNode.getByText(/must have at least 5 characters/i);
-  expect(errorMsg);
-  // console.log(userField);
+  userEvent.type(firstnameField, "Yes");
+  userEvent.click(submitButton);
+  let error = await contactNode.getByText(/must have at least 5 characters/i);
+  expect(error);
 });
 
-test("renders THREE error messages if user enters no values into any fields.", async () => {});
+test("renders THREE error messages if user enters no values into any fields.", async () => {
+  let contactNode = render(<ContactForm />);
+  let submitButton = contactNode.getByTestId(/submitTest/i);
+  userEvent.click(submitButton);
+  let errors = await contactNode.findAllByTestId("error");
+  expect(errors.length === 3);
+});
 
-test("renders ONE error message if user enters a valid first name and last name but no email.", async () => {});
+test("renders ONE error message if user enters a valid first name and last name but no email.", async () => {
+  let contactNode = render(<ContactForm />);
+  let firstnameField = contactNode.getByLabelText(/First Name\*/i);
+  let lastnameField = contactNode.getByLabelText(/Last Name\*/);
+  let submitButton = contactNode.getByTestId(/submitTest/i);
+  userEvent.type(firstnameField, "Taheel");
+  userEvent.type(lastnameField, "Remaarwa");
+  userEvent.click(submitButton);
+  let errors = await contactNode.findAllByTestId("error");
+  expect(errors.length === 1);
+});
 
-test('renders "email must be a valid email address" if an invalid email is entered', async () => {});
+test('renders "email must be a valid email address" if an invalid email is entered', async () => {
+  let contactNode = render(<ContactForm />);
+  let emailField = contactNode.getByLabelText(/Email\*/i);
+  let submitButton = contactNode.getByTestId(/submitTest/i);
+  userEvent.type(emailField, "fainfianfia");
+  userEvent.click(submitButton);
+  let error = await contactNode.findByText(
+    /email must be a valid email address/i
+  );
+  expect(error);
+});
 
-test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {});
+test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
+  let contactNode = render(<ContactForm />);
+  let submitButton = contactNode.getByTestId(/submitTest/i);
+  userEvent.click(submitButton);
+  let error = await contactNode.findByText(/lastName is a required field/i);
+  expect(error);
+});
 
 test("renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.", async () => {});
 
